617d7590703020192a42d69fb1033ce9e62848fa
adrian-swifter <milosffh@yahoo.com>
2022-01-16 22:22:21 +0100

    finished with form markup

--- main.js
+++ main.js
@@ -8,28 +8,5 @@ if ($(".slider").length !== 0) {
   });
 }
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  "use strict";
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll(".needs-validation");
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity() && document.getElementById('validationCustom01').value !== "brando") {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
 
 
