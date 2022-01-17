40d1bdae87bab510805cf389b9ffab2abad7b328
adrian-swifter <milosffh@yahoo.com>
2022-01-17 02:51:03 +0100

    finished validation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// TASK 3 SLIDER
 if ($(".slider").length !== 0) {
   $(".slider").slick({
     autoplay: true,
@@ -8,10 +9,14 @@ if ($(".slider").length !== 0) {
   });
 }
 
+// TASK 2 FORM VALIDATION
 (function () {
   const form = document.getElementById("form");
   const firstName = document.getElementById("first-name");
   const lastName = document.getElementById("last-name");
+  const maleGender = document.getElementById("male");
+  const femaleGender = document.getElementById("female");
+  const birthYear = document.getElementById("birth-year");
   const address = document.getElementById("address");
   const city = document.getElementById("city");
 
@@ -22,39 +27,69 @@ if ($(".slider").length !== 0) {
   });
 
   function checkInputs() {
-    // trim to remove the whitespaces
     const firstNameValue = firstName.value.trim();
     const lastNameValue = lastName.value.trim();
+    const birthYearValue = birthYear.value;
     const addressValue = address.value.trim();
     const cityValue = city.value.trim();
 
+    const regex = new RegExp("^[a-zA-Z0-9]");
+
     if (firstNameValue === "") {
       setErrorFor(firstName, "First name can't be blank");
+    } else if (!regex.test(firstNameValue)) {
+      setErrorFor(firstName, "Special characters aren't allowed");
     } else {
       setSuccessFor(firstName);
     }
 
     if (lastNameValue === "") {
       setErrorFor(lastName, "Last name can't be blank");
+    } else if (!regex.test(firstNameValue)) {
+      setErrorFor(firstName, "Special characters aren't allowed");
     } else {
       setSuccessFor(lastName);
     }
 
+    if (maleGender.checked === false && femaleGender.checked === false) {
+      setErrorFor(maleGender, "Please select your gender");
+    } else if (maleGender.checked === true && femaleGender.checked === true) {
+      setErrorFor(femaleGender, "Please select either male or female gender");
+    } else {
+      setSuccessFor(maleGender.parentElement);
+    }
+
+    if (birthYearValue === "") {
+      setErrorFor(birthYear, "Please choose your year of birth.");
+    } else {
+      setSuccessFor(birthYear);
+    }
+
     if (addressValue === "") {
-      setErrorFor(address, "Last name can't be blank");
+      setErrorFor(address, "Address field can't be blank");
+    } else if (!regex.test(firstNameValue)) {
+      setErrorFor(firstName, "Special characters aren't allowed");
     } else {
       setSuccessFor(address);
     }
 
     if (cityValue === "") {
-      setErrorFor(city, "Last name can't be blank");
+      setErrorFor(city, "City name can't be blank");
+    } else if (!regex.test(firstNameValue)) {
+      setErrorFor(firstName, "Special characters aren't allowed");
     } else {
       setSuccessFor(city);
     }
+
+    console.log(birthYear.value);
   }
 
   function setErrorFor(input, message) {
-    const fieldWrapper = input.parentElement;
+    let fieldWrapper = input.parentElement;
+    if (input.type === "checkbox") {
+      fieldWrapper = input.parentElement.parentElement;
+    }
+
     const errorMsg = fieldWrapper.querySelector(".error-msg");
     fieldWrapper.classList.add("error");
     errorMsg.innerText = message;
